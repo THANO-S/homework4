1e15b1121ea50a1700d7c6f17d1a6abe9b67db69
Christopher Pomposo <chris2017pg@gmail.com>
2025-02-13 19:20:36 -0600

    Completed transaction list component

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -23,7 +23,7 @@ const onSubmit = () => {
 
 <template>
     <h3>Add a new Transaction</h3>
-    <form id="form" @submit.preven="onSubmit">
+    <form id="form" @submit.prevent="onSubmit">
         <div class="form-control">
             <label for="text">Enter Transaction</label>
             <input type ="text" id="text" v-model="text" placeholder="Enter Transaction...">
